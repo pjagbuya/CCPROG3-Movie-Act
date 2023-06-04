5e634845dfe16224a98683cb85278342a4318ace
pjagbuya <54502889+pjagbuya@users.noreply.github.com>
2023-06-05 02:23:21 +0800

    Javadoc
    You forgot to display movie for one more time in the last time in 26, i also changed some parameter names to be in line with the UML

--- Movie.java
+++ Movie.java
@@ -1,2 +1,316 @@
+/** The class Movie represents a movie
+  * available and listed from iTunes
+  *
+  * @author Paul Josef P. Agbuya
+  * @author Vince Kenneth D. Rojo
+  * @author Acemere F. Villena
+  * @version 1.0
+  */
+public class Movie {
 
-//Sampleasfasf
+    
+    /**
+     * This constructor intitializes the title, genre, and the year
+     * the movie was made in the iTunes platform based on the parameters
+     * given. It also checks if the genre provided is valid to be set as 
+     * an attribute else continue the default movie genre as comedy. It would
+     * then check if the year is validly at least 1900 to set or else will 
+     * set default as 2020. Furthermore, initializes an array of actors 
+     * which can only contain a maximum of 5 Strings. Lastly, sets default
+     * value of rentCount to 0 and increments the movieCount
+     * 
+     * @param title the title of the movie
+     * @param genre the classfied genre of the movie
+     * @param year the year the movie was released
+     */
+    Movie(String title, String genre, int year) {
+        this.TITLE = title;
+        this.genre = "comedy";
+        this.year = year;
+        this.actors = new String[5];
+        this.rentCount = 0;
+        movieCount++;
+
+        if(validateGenre(genre)){
+            this.genre = genre;
+        }
+
+        if(year > 1900){
+            this.year = year;
+        }
+        else{
+            this.year = 2020;
+        }
+
+
+    }
+
+    /**
+     * This constructor intitializes the title and genre of the
+     * movie created in the iTunes platform based on the parameters
+     * given. It also checks if the genre provided is valid to be set as 
+     * an attribute else continue the default movie genre as comedy. 
+     * Furthermore, initializes an array of actors which can only 
+     * contain a maximum of 5 Strings. Lastly, sets default value of rentCount
+     * to 0, default value of year to 2020, and increments the movieCount
+     * 
+     * @param title the title of the movie
+     * @param genre the classfied genre of the movie
+     */
+    Movie(String title, String genre) {
+        this(title, genre, 2020);
+    }
+
+
+    /**
+     * This constructor intitializes the title, genre, and one actor in the
+     * movie created in the iTunes platform based on the parameters
+     * given. It also checks if the genre provided is valid to be set as 
+     * an attribute or else continue the default movie genre as comedy. 
+     * Furthermore, initializes an array of actors which can only contain a maximum of
+     * 5 Strings. The parameter actor should be filling the first slot of 
+     * this array. Lastly, sets default value of rentCount to 0, default 
+     * value of year to 2020, and increments the movieCount
+     * 
+     * @param title the title of the movie
+     * @param genre the classfied genre of the movie
+     * @param actor the actor that is within the movie
+     */
+    Movie(String title, String genre, String actor) {
+        this(title, genre, 2020);
+        addActor(actor);
+
+    }
+
+    /**
+     * This constructor intitializes the title and year that the
+     * movie created in the iTunes platform based on the parameters
+     * given. It also sets the default genre of the movie as comedy. 
+     * It would then check if the year is validly at least 1900 to set 
+     * or else will set default as 2020. Furthermore, initializes an 
+     * array of actors which can only contain a maximum of 5 Strings. 
+     * Lastly, sets default value of rentCount to 0 and increments 
+     * the movieCount.
+     * 
+     * @param title the title of the movie
+     * @param year the year the movie was released
+     */
+    Movie(String title, int year) {
+        this(title,"comedy", year);
+    }
+
+
+    /**
+     * This method sets the genre based on the parameters given. It would first
+     * check if it is valid to be set as genre or else does not change the original
+     * genre attribute.
+     * 
+     * @param genre the classfied genre of the movie
+     */
+    public void setGenre(String genre) {
+
+        if(validateGenre(genre))
+            this.genre = genre;
+
+    }
+
+
+    /**
+     * This method sets the year of the movie based on the parameter given. But it
+     * would first check if it is at least from 1900 or else it will not change and retain
+     * the original value of the year
+     * 
+     * @param year the year the movie was released
+     */
+    public void setYear(int year) {
+        if (year >= 1900){
+            this.year = year;
+        }
+        
+    }
+
+
+    /**
+     * This method checks if the parameter can be added to the String
+     * array, if so it would add it in the array. When the array is full
+     * it would then return false, signifying the parameter cannot be added
+     * 
+     * @param actor the actor that is within the movie
+     * @return true if actor can be added to the array, else when the array is full
+     *         this method will return false
+     */
+    public boolean addActor(String actor) {
+        int i;
+        for (i = 0; i < actors.length; i++) {
+            if (actors[i] == null) {
+                actors[i] = actor;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * This method checks if the parameter can be allowed as an attribute of genre from
+     * the set list of genres.
+     * 
+     * @param genre the classfied genre of the movie
+     * @return true if genre is part of the list of genres, else when it is not then
+     *         this method will return false
+     */  
+    public boolean validateGenre (String genre) {
+        int i;
+        String[] genres = new String[]{"drama", "horror", "comedy", "biography",
+            "sci-fi", "action","romance", "kids"};
+
+        for (i = 0; i < genres.length; i++){
+
+            if(genre.equalsIgnoreCase(genres[i])){
+
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+
+    /**
+     * This method increments movieCount to count the number of
+     * created movies
+     */
+    public void rent() {
+        rentCount++;
+    }
+
+    /**
+     * This method checks if the rentCount of the movie is at least
+     * the value of 10 000
+     * 
+     * @return true if the rentCount is greater than or equal to 10000,
+     *         else will return false when less than 10000
+     */
+    public boolean isBlockbuster() {
+        return rentCount >= 10000;
+    }
+
+
+    /**
+     * This method returns the title of the movie
+     * 
+     * @return the String indicating the title of the movie
+     */
+    public String getTitle() {
+        return this.TITLE;
+    }
+
+
+    /**
+     * This method returns the genre the movie is classified as.
+     * 
+     * @return the String indicating the genre classification of the movie.
+     */
+    public String getGenre() {
+        return this.genre;
+    }
+
+
+    /**
+     * This method returns the year the movie was released
+     * 
+     * @return the number that indicates the year of the movie was released
+     */
+    public int getYear() {
+        return this.year;
+    }
+
+
+    /**
+     * This method returns the list of actors in a manner of a String array
+     * 
+     * @return String array indicating the actors of a movie
+     */
+    public String[] getActors() {
+        return actors;
+    }
+
+
+    /**
+     * This method returns the amount of times the movie was rented
+     * 
+     * @return the number of times the movie was rented
+     */
+    public int getRentCount() {
+        return rentCount;
+    }
+
+
+    /**
+     * This method returns the number of times a movie was created by this class
+     * 
+     * @return the number of times a movie was created by this class
+     */
+    public int getMovieCount() {
+        return movieCount;
+    }
+
+
+    /**
+     * This method returns the concatentation of the movie's title, year, and genre
+     * 
+     * For Example: Frozen, 2013, kids
+     * @return concatentation of the movie's title, year, and genre
+     */
+    @Override
+    public String toString() {
+        return TITLE + ", " + year + ", " + genre;
+    }
+
+
+    /**
+     * This method checks the parameter if it is identically being compared to itself
+     * or the parameter's title, genre, and year released are identical to this current
+     * instance of Movie. It returns false if not identical, else it will return true.
+     * 
+     * @param thatMovie is another Movie object that is compared to this current instance of a Movie class
+     * 
+     * @return true if the parameter is identical or the title, genre, and year is the same. This would
+     *         otherwise return false if thatMovie was null or at least one of parameter's attribute formerly
+     *         mentioned is not equal to this current instance's
+     */
+    public boolean equals(Movie thatMovie) {
+
+        if (this == thatMovie){
+            return true;
+        }
+
+        return thatMovie != null && ((this.TITLE).equalsIgnoreCase(thatMovie.TITLE) && 
+               (this.genre).equalsIgnoreCase(thatMovie.genre) && 
+               this.year == thatMovie.year);
+    }
+
+    /**
+     * This method decreases only the movieCount or the attribute indicating the number 
+     * of movies currently available
+     */
+    public static void decrementMovieCount() {
+        movieCount--;
+    }
+
+
+    /** Title of the movie */
+    private final String TITLE;
+    /**Genre that the movie is classfied as */
+    private String genre;
+    /**Year the movie is released */
+    private int year;
+     /**Actors/actresses' name listed under the movie */
+    private String[] actors;
+    /**Number of rent this movie currently has */
+    private int rentCount;
+    /**Total amount of Movie objects made and available currently*/
+    private static int movieCount;
+
+}
--- MovieDriver.java
+++ MovieDriver.java
@@ -1,1 +1,127 @@
+import java.util.Arrays;
+/**
+ * Class MovieDriver is a driver class that is used
+ * to create instances of the Movie Class in simulating
+ * its use in creating Movies in the iTunes
+ */
+public class MovieDriver {
+    public static void main(String[] args) {
 
+        // Declare and instantiate an array of 5 Movie objects called mustWatch
+        Movie[] mustWatch = new Movie[5];
+        int i, j;
+
+        /* Instantiate the first Movie object using the following information:
+           title = X-Men , genre = thriller, year = 2000 */
+        mustWatch[0] = new Movie("X-Men","thriller",2020);
+
+        /* Instantiate the second Movie object using the following information:
+           title = Pokemon the movie , genre = kids*/
+        mustWatch[1] = new Movie("Pokemon the movie","kids");
+
+        /* Instantiate the third Movie object using the following information:
+           title = Thor , genre = action , actor = Chris Hemsworth*/
+        mustWatch[2] = new Movie("Thor","action","Chris Hemsworth");
+
+        /* Instantiate the fourth Movie object using the following information:
+           title = Frozen 2 , year = 2019*/
+        mustWatch[3] = new Movie("Frozen 2",2019);
+
+        // Display the total number of movies instantiated
+        System.out.println("Number of movies instantiated: " + mustWatch[0].getMovieCount());
+
+        // Display the title, genre, and year of all movies in mustWatch
+        for (i = 0; i < mustWatch.length; i++) {
+            if (mustWatch[i] != null)
+                System.out.println(mustWatch[i].getTitle() + ", " + mustWatch[i].getGenre() + ", " + mustWatch[i].getYear());
+        }
+
+        // Modify genre of first movie in mustWatch to sci-f
+        mustWatch[0].setGenre("sci-fi");
+
+        // Modify genre of 4th movie in mustWatch to kids
+        mustWatch[3].setGenre("kids");
+
+        // Modify year released of second movie in mustWatch to 2019
+        mustWatch[1].setYear(2019);
+
+        // Modify year released of third movie in mustWatch to 2011
+        mustWatch[2].setYear(2011);
+
+
+        // Display the title, genre, and year of all movies in mustWatch
+        for (i = 0; i < mustWatch.length; i++) {
+            if (mustWatch[i] != null)
+                System.out.println(mustWatch[i].getTitle() + ", " + mustWatch[i].getGenre() + ", " + mustWatch[i].getYear());
+        }
+
+        // Add Natalie Portman as another actor to the third movie stored in mustWatch
+        mustWatch[2].addActor("Natalie Portman");
+
+        /* Add the following actors (in sequence) to the second movie stored in mustWatch:
+           Pikachu, Charizard, Squirtle, Ditto, Mewtwo, Psyduck */
+        mustWatch[1].addActor("Pikachu");
+        mustWatch[1].addActor("Charizard");
+        mustWatch[1].addActor("Squirtle");
+        mustWatch[1].addActor("Ditto");
+        mustWatch[1].addActor("Mewtwo");
+        mustWatch[1].addActor("Psyduck");
+
+        // Display all actors of the third movie stored in mustWatch
+        System.out.println("The actors of Pokemon are:" + Arrays.toString(mustWatch[2].getActors()));
+
+        // Display all actors of the second movie stored in mustWatch
+        System.out.println("The actors of Thor are: " + Arrays.toString(mustWatch[1].getActors()));
+        
+        // Declare and instantiate another array of at most 3 movies called myRenta
+        Movie[] myRental = new Movie[3];
+
+        /* Rent all movies that are in the genre of kids from mustWatch. These movies should now be 
+           associated also to myRental */
+        j = 0;
+        for (i = 0; i < mustWatch.length; i++) {
+            if (mustWatch[i] != null && mustWatch[i].getGenre().equalsIgnoreCase("kids")) {
+                mustWatch[i].rent();
+                myRental[j] = mustWatch[i];
+                j++;
+            }
+        }
+
+        // Display the total number of movies instantiated
+        System.out.println("Total number of movies instantiated: " + myRental[0].getMovieCount());
+
+        //  Display the title, genre, and year of all movies in mustWatch 
+        for (i = 0; i < mustWatch.length; i++) {
+            if (mustWatch[i] != null)
+                System.out.println(mustWatch[i].getTitle() + ", " + mustWatch[i].getGenre() + ", " + mustWatch[i].getYear());
+        }
+
+        // Display the title, genre, and year of all movies in myRental
+        for (i = 0; i < myRental.length; i++) {
+            if (myRental[i] != null)
+                System.out.println(myRental[i].getTitle() + ", " + myRental[i].getGenre() + ", " + myRental[i].getYear());
+        }
+
+        // Rent the movie Frozen 2 again and store that as the last element to myRental
+        myRental[myRental.length - 1] = mustWatch[3];
+
+        /* Display the number of times the movie stored in the second element of myRental was rented
+           and display the result of isBlockBuster() for that movie */
+        System.out.println("Number of times the movie stored in the second element of myRental was rented: " + myRental[1].getRentCount());
+        System.out.println("Is it blockbuster? " + myRental[1].isBlockbuster());
+
+        // Set myRental to null
+        myRental = null;
+
+        // Delete the movie object with title X-Men
+        mustWatch[0] = null;
+
+        // Update the total number of movies instantiated. Display the total number of movies instantiated
+        Movie.decrementMovieCount();
+        System.out.println("Total number of movies instantiated: " + mustWatch[1].getMovieCount());
+
+        // Clean up
+        mustWatch = null;
+
+    }
+}
